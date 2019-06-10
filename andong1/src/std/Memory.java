21b56c33c8c84468c60ac2c626aae5732f18ab5c
tjdanqkr <49061542+tjdanqkr@users.noreply.github.com>
2019-06-10 19:04:35 +0900

    rw
    fsfs

---
+++ andong1/src/std/Memory.java
@@ -0,0 +1,17 @@
+package std;
+
+public class Memory implements MyStack,MyQueue{
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ andong1/src/std/MyStack.java
@@ -0,0 +1,7 @@
+package std;
+
+public interface MyStack {
+	
+	public void push();
+	public void pop();
+}
